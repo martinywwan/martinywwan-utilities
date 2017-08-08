e0e7cf1c28394a8f4be256ca741f36981a4459d4
martinywwan <martin.yw.wan2@hotmail.com>
2017-08-08 20:13:20 +0100

    added comments

--- src/main/java/com/martinywwan/util/StringUtil.java
+++ src/main/java/com/martinywwan/util/StringUtil.java
@@ -3,7 +3,8 @@ package com.martinywwan.util;
 import java.io.StringWriter;
 import java.util.Arrays;
 
-/**Collection of methods to perform common functions
+/**
+ * Collection of methods to perform common functions
  *
  * @author martinywwan
  */
@@ -14,28 +15,29 @@ public class StringUtil {
             {33, 47}, {58,64}, {91, 96}, {123, 126}
     };
 
-    /**Checks whether the input character is a punctuation
+    /**
+     * Checks whether the input character is a punctuation
      *
-     * @param c is the character input
+     * @param charInput is the character input
      * @param punctuationToKeep is the array of punctuation to preserve
      * @return true if character input is a punctuation
      */
-    public final static boolean isPunctuation(Character c, Character ... punctuationToKeep){
-
-        if(Arrays.asList(punctuationToKeep).contains(c)){ return false; }
+    public final static boolean isPunctuation(Character charInput, Character ... punctuationToKeep){
+        if(Arrays.asList(punctuationToKeep).contains(charInput)){ return false; }
 
         for(int i = 0; i<PUNCTUATION_RANGE.length; i++){
-            if( ((int)c.charValue()>=PUNCTUATION_RANGE[i][0]) && ((int)c.charValue()<=PUNCTUATION_RANGE[i][1])){
+            if( ((int)charInput.charValue()>=PUNCTUATION_RANGE[i][0]) && ((int)charInput.charValue()<=PUNCTUATION_RANGE[i][1])){
                 return true;
             }
         }
         return false;
     }
 
-    /** Strips out all punctuation in a string but preserves when stated
-     * @param orig - input String
-     * @param punctuationToKeep - array of punctuation to keep
-     * @return a String without punctuation
+    /**
+     * Strips out all punctuation in a string but preserves when stated
+     * @param orig is the input String
+     * @param punctuationToKeep is the punctuation to keep in the original input String
+     * @return a String without any punctuation unless preserved
      */
     public final static String removePunctuation(String orig, Character ... punctuationToKeep){
         Character[] charObjectArray = orig.chars().mapToObj(c -> (char)c).toArray(Character[]::new);
--- src/test/java/com/martinywwan/util/TestStringUtil.java
+++ src/test/java/com/martinywwan/util/TestStringUtil.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-//TODO test with all punctuations
+//TODO Need to define more test cases e.g. variable PUNCTUATION_RANGE contains only punctuation
 public class TestStringUtil {
 
     private String testInputString;
@@ -19,18 +19,21 @@ public class TestStringUtil {
         punctuationsToKeep = new Character[] { '#' } ;
     }
 
+    //Test if the punctuation input is valid (is accepted as a punctuation)
     @Test
     public void isPunctuation_True_NoFilter(){
         assertTrue(StringUtil.isPunctuation('#'));
         assertTrue(StringUtil.isPunctuation(','));
     }
 
+    //Test that the character input is not a punctuation
     @Test
     public void isPunctuation_False_NoFilter(){
         assertFalse(StringUtil.isPunctuation('a'));
         assertFalse(StringUtil.isPunctuation('P'));
     }
 
+    //Check whether the input punctuation is a valid punctuation
     @Test
     public void isPunctuation_True_WithFilter(){
         Character inputChar = '@';
@@ -41,6 +44,8 @@ public class TestStringUtil {
         assertTrue(StringUtil.isPunctuation(inputChar, toKeepConditions));
     }
 
+
+    //Check whether the input punctuation is not filtered out when we want to keep it
     @Test
     public void isPunctuation_False_WithFilter(){
         Character inputChar = '#';
@@ -51,12 +56,15 @@ public class TestStringUtil {
         assertFalse(StringUtil.isPunctuation(inputChar, toKeepConditions));
     }
 
+    //Test that all punctuation is removed from the given input string
     @Test
     public void removePunctuation_NoFilter(){
         String expected = "The new IT movie adaption looks so scary Looking forward to watching it in Odeon hopefully it is not too pricey TangoIceBlasters SweetPopcorn odeon";
         assertEquals(expected, StringUtil.removePunctuation(testInputString));
     }
 
+
+    //Test that all punctuation is removed from the given input string apart from '#'
     @Test
     public void removePunctuation_WithFilter(){
         String expected = "The new IT movie adaption looks so scary Looking forward to watching it in Odeon hopefully it is not too pricey #TangoIceBlasters #SweetPopcorn #odeon";
